7b8a879b43fcb13a822ffc1387d7bcac543c8c74
ranedigamber <rane.24@osu.edu>
2022-08-18 18:36:12 -0400

    Updates

--- static/js/app.js
+++ static/js/app.js
@@ -21,5 +21,28 @@ function buildTable(data) {
 );
 });
 }
+// Create a filter to handle clicks
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filterData = tableData;
+    // Check to see if a date was entered and filter the
+    // data usin that date
+    if (date) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, the filteredData will
+    // just be the original tableData.
+    buildTable(filterData);
+}
 
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//Build the table when the page loads
+buildTable(data);
+ 
 
